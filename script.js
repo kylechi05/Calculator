bf62acf6bdc6d9aa44faf4fea28978746c88f1b9
kylechi05 <kylechi2157@gmail.com>
2023-11-02 10:46:36 -0500

    lol finally done

--- script.js
+++ script.js
@@ -1,9 +1,57 @@
 const buttons = document.querySelectorAll(".buttons");
+const displayer = document.querySelector(".display-bar");
 
 for (let button of buttons) {
     button.addEventListener('click', () => display(button.innerHTML));
 }
 
+let stack = [];
+
 function display(x) {
-    console.log(x);
+    switch(x) {
+        case "AC":
+            displayer.innerHTML = null;
+            stack = [];
+            break;
+        case "CE":
+            displayer.innerHTML = displayer.innerHTML.slice(0, -1);
+            stack.pop();
+            break;
+        case "=":
+            calculate(stack);
+            break;
+        default:
+            displayer.innerHTML += x;
+            switch(x) {
+                case "÷":
+                    stack.push("/");
+                    break;
+                case "×":
+                    stack.push("*");
+                    break;
+                case "-":
+                    stack.push("-");
+                    break;
+                case "+":
+                    stack.push("+");
+                    break;
+                default:
+                    stack.push(x);
+            }
+    }
+}
+
+function calculate(x) {
+    try {
+        let answer = eval(x.join(""));
+        console.log(answer);
+        stack = [answer];
+        displayer.innerHTML = answer;
+    }
+    catch (error) {
+        console.log(error);
+        displayer.innerHTML = error;
+    }
+
+    
 }
